9771e9c80fa62cd26fee3a1cbed2ff3385a93787
Svetlana Bushmakina <svetik_5073@mail.ru>
2022-10-30 22:16:32 +0400

    SarumanAndLannister

---
+++ src/main/java/SarumanAndLannister.java
@@ -0,0 +1,14 @@
+public class SarumanAndLannister {
+    public static void main(String[] args) {
+
+        final int warriorLannister = 1234;
+        System.out.println("У Ланнистера в войске " + warriorLannister + " пехотинцев");
+
+        final int orcsSaruman = 1543;
+        System.out.println("Войско Сарумана составляет " + orcsSaruman + " орков");
+
+        final int unionSarumanAndLannister = warriorLannister + orcsSaruman;
+        System.out.println("В союзе Сарумана и Ланнистера " + unionSarumanAndLannister + " пехотинцев");
+
+    }
+}
